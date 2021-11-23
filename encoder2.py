4008f4c7140b5fb15bbcaea61b74ada3a01e706f
Dreadtristan <94937729+Dreadtristan@users.noreply.github.com>
2021-11-23 18:27:23 -0500

    Add files via upload

---
+++ encoder2.py
@@ -0,0 +1,130 @@
+
+coder = []
+coder2 = []
+loop1 = True
+count = 0
+loop2 = True
+loop22 = 0
+loop3 = True
+loop4 = True
+while loop1 == True:
+    inpot = input("encoder: ")
+    count += 1
+    if inpot == "stop":
+        loop1 = False
+    if inpot == "a":
+        coder.append("t")
+        coder2.append("b")
+    if inpot == "b":
+        coder.append("n")
+        coder2.append("c")
+    if inpot == "c":
+        coder.append("8")
+        coder2.append("7")
+    if inpot == "d":
+        coder.append("v")
+        coder2.append("q")
+    if inpot == "e":
+        coder.append("g")
+        coder2.append("9")
+    if inpot == "f":
+        coder.append("b")
+        coder2.append("w")
+    if inpot == "g":
+        coder.append("d")
+        coder2.append("d")
+    if inpot == "h":
+        coder.append("s")
+        coder2.append("2")
+    if inpot == "i":
+        coder.append("i")
+        coder2.append("r")
+    if inpot == "j":
+        coder.append("5")
+        coder2.append("m")
+    if inpot == "k":
+        coder.append("f")
+        coder2.append("n")
+    if inpot == "l":
+        coder.append("2")
+        coder2.append("s")
+    if inpot == "m":
+        coder.append("q")
+        coder2.append("l")
+    if inpot == "n":
+        coder.append("h")
+        coder2.append("k")
+    if inpot == "o":
+        coder.append("m")
+        coder2.append("i")
+    if inpot == "p":
+        coder.append("k")
+        coder2.append("0")
+    if inpot == "q":
+        coder.append("p")
+        coder2.append("u")
+    if inpot == "r":
+        coder.append("7")
+        coder2.append("f")
+    if inpot == "s":
+        coder.append("e")
+        coder2.append("1")
+    if inpot == "t":
+        coder.append("9")
+        coder2.append("v")
+    if inpot == "u":
+        coder.append("4")
+        coder2.append("5")
+    if inpot == "v":
+        coder.append("l")
+        coder2.append("3")
+    if inpot == "w":
+        coder.append("o")
+        coder2.append("4")
+    if inpot == "x":
+        coder.append("6")
+        coder2.append("y")
+    if inpot == "y":
+        coder.append("y")
+        coder2.append("z")
+    if inpot == "z":
+        coder.append("z")
+        coder2.append("a")
+    if inpot == "1":
+        coder.append("0")
+        coder2.append("8")
+    if inpot == "2":
+        coder.append("c")
+        coder2.append("6")
+    if inpot == "3":
+        coder.append("3")
+        coder2.append("p")
+    if inpot == "4":
+        coder.append("1")
+        coder2.append("h")
+    if inpot == "5":
+        coder.append("r")
+        coder2.append("t")
+    if inpot == "6":
+        coder.append("x")
+        coder2.append("g")
+    if inpot == "7":
+        coder.append("u")
+        coder2.append("x")
+    if inpot == "8":
+        coder.append("a")
+        coder2.append("e")
+    if inpot == "9":
+        coder.append("w")
+        coder2.append("o")
+    if inpot == "0":
+        coder.append("!")
+        coder2.append("j")
+    if inpot == "print":
+        print("core")
+        print(coder)
+        print("encoder")
+        print(coder2)
+    if inpot == "clear":
+        coder = []
+        coder2 = []
